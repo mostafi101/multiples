41c67cf54680f6757307ae52d0d27b7045be9704
mostafij101 <p.mostafij@gmail.com>
2016-06-04 08:29:26 -0500

    App is finished

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,15 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Properties
+    var addVal = 0
+    var addSum = 0
+    var counter = 0
+    
+    //Outlet
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var multipelTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var multipleLbl: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
+        
+        if multipelTxt.text != nil && multipelTxt.text != ""{
+            logo.hidden = true
+            multipelTxt.hidden = true
+            playBtn.hidden = true
+            
+            multipleLbl.hidden = false
+            addBtn.hidden = false
+            
+            addVal = Int(multipelTxt.text!)!
+            
+            updateMultipleLbl()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onAddBtnPressed(sender: UIButton!){
+        if counter <= 5{
+            addSum = sum()
+            updateMultipleLbl()
+            counter += 1
+        }else{
+            restart()
+        }
+    
+    }
+    
+    func updateMultipleLbl(){
+        multipleLbl.text = "\(addSum) + \(addVal) = \(sum())"
+    }
+    
+    func sum() -> Int{
+        return addSum + addVal
+    }
+    
+    func restart(){
+        counter = 0
+        addSum = 0
+        multipelTxt.text = ""
+        logo.hidden = false
+        multipelTxt.hidden = false
+        playBtn.hidden = false
+        
+        multipleLbl.hidden = true
+        addBtn.hidden = true
     }
 
 
